3d58f0a41547f1440bc1d72eaa681fa4e012ea77
Timc233 <timchen@bu.edu>
2024-01-27 13:59:05 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, value):
+        return value
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, value):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) + self.p2.evaluate(value)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -27,6 +36,9 @@ class Sub:
     
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) - self.p2.evaluate(value)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -39,6 +51,10 @@ class Mul:
         p2_str = "(" + repr(self.p2) + ")" if isinstance(self.p2, Add) or isinstance(self.p2, Sub) else repr(self.p2)
 
         return p1_str + " * " + p2_str
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) * self.p2.evaluate(value)
+
 
 
 class Div:
@@ -52,13 +68,22 @@ class Div:
 
         return p1_str + " / " + p2_str
     
+    def evaluate(self, value):
+        return self.p1.evaluate(value) / self.p2.evaluate(value)
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Div( Int(1), Sub( Mul(X(), X()), Int(1)))))
 print(poly)
+print(poly.evaluate(2))
 
 poly_2 = Add(Div(Mul(X(), X()), Add(Int(25), X())), Mul(Sub(X(), Int(26)), Add(X(), Int(27))))
 print(poly_2)
+print(poly_2.evaluate(2))
 
 poly_3 = Mul(Add(Sub(Div(Mul(Int(39), Int(40)), Add(Int(41), Int(42))), Int(43)), Int(44)), X())
 print(poly_3)
+print(poly_3.evaluate(2))
+
+poly_4 = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+print(poly_4)
+print(poly_4.evaluate(-1))
